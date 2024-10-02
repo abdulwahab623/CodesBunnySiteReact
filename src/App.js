b5bd67371105e742a90fe8cdb562bb443277d3d3
abdul <abdulwahab95623@gmail.com>
2024-10-02 12:45:54 -0700

    gg

--- src/App.js
+++ src/App.js
@@ -22,19 +22,22 @@ import Blogthree from "./component/Blogthree";
 import Blogfour from "./component/Blogfour";
 import Blogfive from "./component/Blogfive";
 import Blogsix from "./component/Blogsix";
+import Application from "./component/Mobapplication";
+import SEO from "./component/SEO";
+
 function App() {
 
   return (
     <>
       <Navbar />
       <div class="social-icons">
-        <a href="https://linkedin.com" target="_blank" className="mail" style={{ color: 'black' }}><img src={Mail} style={{ width: '30px' }} alt="" /> <span>&nbsp;Mail</span></a>
+        <a href="mailto:officialcodesbunny@gmail.com" target="_blank" className="mail" style={{ color: 'black' }}><img src={Mail} style={{ width: '30px' }} alt="" /> <span>&nbsp;Mail</span></a>
 
         <a href="https://linkedin.com" target="_blank" class="linkedin"><i class="fab fa-linkedin"> </i> <span>&nbsp;LinkedIn</span></a>
         <a href="https://wa.me/923080044190?text=Hello%2C%20I%27m%20interested%20in%20your%20services" target="_blank" class="whatsapp"><i class="fab fa-whatsapp"> </i> <span>&nbsp;Whatsapp</span></a>
 
-        <a href="https://facebook.com" target="_blank" class="facebook"><i class="fab fa-facebook"> </i> <span>&nbsp;Facebook</span></a>
-        <a href="https://instagram.com" target="_blank" class="instagram"><i class="fab fa-instagram"> </i> <span>&nbsp;Instagram</span></a>
+        <a href="https://www.facebook.com/profile.php?id=61565837832961" target="_blank" class="facebook"><i class="fab fa-facebook"> </i> <span>&nbsp;Facebook</span></a>
+        <a href="https://www.instagram.com/official_codesbunny/" target="_blank" class="instagram"><i class="fab fa-instagram"> </i> <span>&nbsp;Instagram</span></a>
       </div>
 
       <div id="whatsapp-button">
@@ -44,6 +47,8 @@ function App() {
         </a>
 
       </div>
+
+      <a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="fa fa-angle-up"></i></a>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
@@ -51,9 +56,11 @@ function App() {
         <Route path='/blog' element={<Blogs />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/web' element={<Web />} />
-        <Route path='/Hms' element={<HmsErp />} />
+        <Route path='/Application' element={<Application />} />
+        <Route path='/OURPRODUCTS' element={<HmsErp />} />
         <Route path='/Content' element={<Content />} />
-        <Route path='/SeoAso' element={<SeoAso />} />
+        <Route path='/ASO' element={<SeoAso />} />
+        <Route path='/SEO' element={<SEO />} />
         <Route path='/Dataandcyber' element={<Datacyber />} />
         <Route path='/Blogone' element={<Blogonetwo />} />
         <Route path='/Blogtwo' element={<Blogtwo />} />
@@ -61,6 +68,7 @@ function App() {
         <Route path='/Blogfour' element={<Blogfour />} />
         <Route path='/Blogfive' element={<Blogfive />} />
         <Route path='/Blogsix' element={<Blogsix />} />
+        
       </Routes>
 
       <Footer />
